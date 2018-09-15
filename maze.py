5fd1025f4226d0c462905c67fbf77393d00b58e5
Kariss83 <vache.romain@gmail.com>
2018-09-15 20:05:32 +0200

    A working class that allows to create a level from a a csv file as been created in the maze.py file

--- maze.py
+++ maze.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-class Position:
 
-    def __init__(self, longitude, latitude):
-        self.longi = longitude
-        self.latit = latitude
+import csv
 
 
 class Maze:
+    """This class allows to read the  structure of the game lvl itself : the 
+    maze from a csv file"""
 
-    NUMBER_OF_LINES = 16
-    ZONES = []
-    
-    def __init__(self,corner1, corner2):
-        self.corner1 = corner1
-        self.corne2 = corner2
-    
-    @classmethod
-    def initialize_zones(cls):
-        for latitude in range(1, cls.NUMBER_OF_LINES, 1):
-            for longitude in range(1, cls.NUMBER_OF_LINES, 1):
-                    bottom_left_corner = Position(longitude, latitude)
-                    top_right_corner = Position(longitude + 1, latitude + 1)
-                    zone = Maze(bottom_left_corner, top_right_corner)
-                    cls.ZONES.append(zone)
-        print(len(cls.ZONES))
+    def __init__(self, data_file): 
+        """the data_file expected is the one containing the maze stucture
+        in a csv file with ';' as delimiter"""
+        self.file = data_file
+        self.structure = []
+
+    def lvl_creation(self):
+        """This method will allow the reading and and use of the csv file
+        in order to build and store in the attribute 'structure' the maze 
+        as a set of values (the one for each sprite)"""
+        with open(self.file, "r") as mazestructure:
+            lvl_structure = []
+            reader = csv.reader(mazestructure, delimiter=';')
+            for lines in reader:
+                line_structure = []
+                for sprite in lines:
+                    line_structure.append(sprite)
+                lvl_structure.append(line_structure)
+            self.stucture = lvl_structure
+
 
 def main():
-    Maze.initialize_zones()
+    maze = Maze('data/maze_structure.csv')
+    maze.lvl_creation()
+
         
 if __name__ == "__main__":
     main()
