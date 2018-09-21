3973c1c539b606da2a97da844892cb4dcfb46886
Kariss83 <vache.romain@gmail.com>
2018-09-21 22:48:55 +0200

    In the main_clean file is a consol version of the project that is almost working but as to be degugged as you can't pick up all the items atm

--- items.py
+++ items.py
@@ -2,25 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import random
+import pygame
 
+
+PIXELS_PER_SPRITE = 120
 
 class Item:
     """This class will allow the creation of any item that lies on the game
     structure (object or NPC)"""
     
-    def __init__(self, lvl, sprite):
+    def __init__(self, lvl, sprite, name):
         self.case_x = 0
         self.case_y = 0
-        self.sprite = sprite
+        #self.sprite = pygame.image.load(sprite).convert()
         self.level = lvl
+        self.name = name
 
-    def placing_items(self, lvl):
+    def placing(self, lvl):
 
         random.seed()
-        k = random.randint(0, len(lvl.empty) -1)
-        self.case_x = lvl.empty[k][0]
-        self.case_y = lvl.empty[k][1]
-        del(lvl.empty[k])
+        k = random.randint(0, len(lvl.empty_for_obj) -1)
+        self.case_x = lvl.empty_for_obj[k][0]
+        self.case_y = lvl.empty_for_obj[k][1]
+        lvl.items.append((self.case_x, self.case_y))
+        del(lvl.empty_for_obj[k])
+        lvl.structure[self.case_x][self.case_y] = self.name
+    
+        
+        
+    def display_item(self, window):
+       #x = self.case_x * PIXELS_PER_SPRITE
+       #y = self.case_y * PIXELS_PER_SPRITE
+       #window.blit(self.sprite, (x,y))
+       pass
 
 
 def main() :
--- maze.py
+++ maze.py
@@ -3,6 +3,9 @@
 
 
 import csv
+import pygame
+
+PIXELS_PER_SPRITE = 120
 
 
 class Maze:
@@ -15,7 +18,12 @@ class Maze:
         self.file = data_file
         self.structure = []
         self.empty = []
-
+        self.empty_for_obj = []
+        self.entry = []
+        self.exit = []
+        self.items = []
+ 
+    
     def lvl_creation(self):
         """This method will allow the reading and and use of the csv file
         in order to build and store in the attribute 'structure' the maze 
@@ -24,30 +32,63 @@ class Maze:
             lvl_structure = []
             line_number = 0
             reader = csv.reader(mazestructure, delimiter=';')
+            
             for lines in reader:
                 line_structure = []
                 sprite_number = 0
+                
                 for sprite in lines:
                     line_structure.append(sprite)
                     if sprite == 'o':
                         self.empty.append((line_number,sprite_number))
-                    sprite_number += 1
-                lvl_structure.append(line_structure)
+                        self.empty_for_obj.append((line_number,sprite_number))
+                    elif sprite == 'entry':
+                        self.entry.append((line_number, sprite_number))
+                    elif sprite == 'exit':
+                        self.exit.append((line_number, sprite_number))
+                        self.empty.append((line_number, sprite_number))
+                    sprite_number += 1                
                 line_number += 1
-            self.stucture = lvl_structure
+                lvl_structure.append(line_structure)
+            self.structure = lvl_structure
 
-    def lvl_display(self):
+
+    def lvl_display(self, window, wall_img, guardian_img):
         """This method will allow to load graphical representation of the maze
         structure that we generated with the lvl_creation method in a window"""
         #associer à chaque lettre une image
-        #parcourir self.structure pour associer à chaque sprite une immage
+        #parcourir self.structure pour associer à chaque sprite une image
+        #chargement des images des murs
+        wall = pygame.image.load(wall_img).convert()
+        
+        #chargement de l'image de la sortie
+        exit = pygame.image.load(guardian_img).convert()
+        
+        #we go through the level structure to give each position its sprite
+        line_number = 0
+        for ligne in self.structure:
+			#On parcourt les listes de lignes
+            case_number = 0
+            for sprite in ligne:
+				#we compute the position in pixel
+                x = case_number * PIXELS_PER_SPRITE
+                y = line_number * PIXELS_PER_SPRITE
+                if sprite == 'x':		   #x = wall
+                    window.blit(wall, (x,y))
+                elif sprite == 'exit':		   
+                    window.blit(exit, (x,y))
+                case_number += 1
+            line_number += 1
 
 
 
 def main():
     maze = Maze('data/maze_structure.csv')
     maze.lvl_creation()
+    print(maze.structure)
     print(maze.empty)
+    print(maze.exit)
+    print(maze.entry)
 
         
 if __name__ == "__main__":
