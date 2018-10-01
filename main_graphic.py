df66aeb1574350bfd70ac761bafd542bf13a3f7f
Kariss <vache.romain@gmail.com>
2018-10-01 18:18:26 +0200

    added a separated constants.py file for all the projet constants

--- main_graphic.py
+++ main_graphic.py
@@ -12,31 +12,7 @@ from pygame.locals import *
 from player import Player
 from maze import Maze
 from items import Item
-
-"""Here are all the constants we need in the game"""
-# window parameter
-SPRITE_NUMBER = 15
-PIXELS_PER_SPRITE = 60
-WINDOW_SIZE = SPRITE_NUMBER * PIXELS_PER_SPRITE
-# window tuning
-WINDOW_TITLE = "Escape game MacGyver"
-ICON_IMAGE = 'data/ressource/tile-crusader-logo.png'
-# game images
-LOBBY_IMAGE = "data/ressource/accueil.png"
-BACKGROUND_IMAGE = "data/ressource/fond.jpg"
-WALL_IMAGE = "data/ressource/mur.png"
-GARDIAN_IMAGE = "data/ressource/Gardien.png"
-HERO_IMAGE = "data/ressource/MacGyver.png"
-ETHER_SPRITE = "data/ressource/ether.png"
-NEEDLE_SPRITE = "data/ressource/aiguille.png"
-PLASTIC_TUBE_SPRITE = "data/ressource/tube_plastique.png"
-INVENTORY_LIST = ["data/ressource/inv0.png",
-                  "data/ressource/inv1.png",
-                  "data/ressource/inv2.png",
-                  "data/ressource/inv3.png"]
-WIN = "data/ressource/win.png"
-LOSS = "data/ressource/loss.png"
-
+from constants import *
 
 """And now the program itself"""
 # pygame start
@@ -180,3 +156,6 @@ while is_running:
                 time.sleep(5.5)
                 # And restart the game
                 remain_in_game = False
+
+if __name__ == "__main__":
+    main()
