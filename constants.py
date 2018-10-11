8c9fe7e9413426b35456b6dc31d3b3f23b2ceb48
Kariss <vache.romain@gmail.com>
2018-10-12 00:12:54 +0200

    replace ressources by resources in constants.py

--- constants.py
+++ constants.py
@@ -10,21 +10,21 @@ PIXELS_PER_SPRITE = 60
 WINDOW_SIZE = SPRITE_NUMBER * PIXELS_PER_SPRITE
 # window tuning
 WINDOW_TITLE = "Escape game MacGyver"
-ICON_IMAGE = 'data/ressource/tile-crusader-logo.png'
+ICON_IMAGE = 'data/resource/tile-crusader-logo.png'
 # game images
-LOBBY_IMAGE = "data/ressource/accueil.png"
-BACKGROUND_IMAGE = "data/ressource/fond.jpg"
-WALL_IMAGE = "data/ressource/mur.png"
-GARDIAN_IMAGE = "data/ressource/Gardien.png"
-HERO_IMAGE = "data/ressource/MacGyver.png"
-ETHER_SPRITE = "data/ressource/ether.png"
-NEEDLE_SPRITE = "data/ressource/aiguille.png"
-PLASTIC_TUBE_SPRITE = "data/ressource/tube_plastique.png"
-INVENTORY_LIST = ["data/ressource/inv0.png",
-                  "data/ressource/inv1.png",
-                  "data/ressource/inv2.png",
-                  "data/ressource/inv3.png"]
-WIN = "data/ressource/win.png"
-LOSS = "data/ressource/loss.png"
+LOBBY_IMAGE = "data/resource/accueil.png"
+BACKGROUND_IMAGE = "data/resource/fond.jpg"
+WALL_IMAGE = "data/resource/mur.png"
+GARDIAN_IMAGE = "data/resource/Gardien.png"
+HERO_IMAGE = "data/resource/MacGyver.png"
+ETHER_SPRITE = "data/resource/ether.png"
+NEEDLE_SPRITE = "data/resource/aiguille.png"
+PLASTIC_TUBE_SPRITE = "data/resource/tube_plastique.png"
+INVENTORY_LIST = ["data/resource/inv0.png",
+                  "data/resource/inv1.png",
+                  "data/resource/inv2.png",
+                  "data/resource/inv3.png"]
+WIN = "data/resource/win.png"
+LOSS = "data/resource/loss.png"
 # Game constants :
 NUMBER_OF_ITEMS = 3
